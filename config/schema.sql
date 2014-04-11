701e8691f711ce552a60af92d3d2c65956d100cc
Franco Brusatti <fbrusatti@gmail.com>
2014-04-11 00:06:53 -0300

    Add initial sql schema

---
+++ config/schema.sql
@@ -0,0 +1,10 @@
+-- Integrantes: W - X - Y - Z
+
+DROP TABLE IF EXISTS users; -- Usuarios
+CREATE TABLE users(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    email VARCHAR(60) UNIQUE,
+    first_name VARCHAR(56),
+    last_name VARCHAR(56),
+  CONSTRAINT users_pk PRIMARY KEY (id)
+);
