691f600d0f11c30f221ddeb7464dea367159c8b1
max1y0 <max1y0@live.com.ar>
2014-04-28 20:48:00 -0300

    Updated SQL- Not finished

--- config/schema.sql
+++ config/schema.sql
@@ -6,5 +6,29 @@ CREATE TABLE users(
     email VARCHAR(60) UNIQUE,
     first_name VARCHAR(56),
     last_name VARCHAR(56),
+	address_id INT(11),
   CONSTRAINT users_pk PRIMARY KEY (id)
 );
+
+CREATE TABLE address (
+	id INT (11) NOT NULL AUTO_INCREMENT,
+	street VARCHAR (120),
+	address_number INT(5),
+CONSTRAINT address_pk PRIMARY KEY (id),
+);
+
+CREATE TABLE vehicles(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	model INT (11),
+	km INT (11),
+	user_id INT(11),
+CONSTRAINT vehicle_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE questions(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	description VARCHAR (200),
+	user_id INT(11),
+CONSTRAINT questions_id PRIMARY KEY (id)
+);
+
