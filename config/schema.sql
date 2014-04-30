821e270ab286c566756bfa253218a2154587e27b
Angelo Chaves <angeloechaves@gmail.com>
2014-04-29 22:10:21 -0300

    Update schema. All tables

--- config/schema.sql
+++ config/schema.sql
@@ -10,25 +10,63 @@ CREATE TABLE users(
   CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE address (
+CREATE TABLE addresses(
 	id INT (11) NOT NULL AUTO_INCREMENT,
-	street VARCHAR (120),
+	street VARCHAR(120),
 	address_number INT(5),
-CONSTRAINT address_pk PRIMARY KEY (id),
+  CONSTRAINT addresses_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE vehicles(
 	id INT(11) NOT NULL AUTO_INCREMENT,
-	model INT (11),
-	km INT (11),
 	user_id INT(11),
-CONSTRAINT vehicle_pk PRIMARY KEY (id)
+    model INT(4),
+    km INT(7),
+  CONSTRAINT vehicles_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE cars(
+    vehicle_id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11),
+    type ENUM('sedan','compact','coupe','wagon','sports'),
+  CONSTRAINT cars_pk PRIMARY KEY (vehicle_id)
+);
+
+CREATE TABLE trucks(
+    vehicle_id INT(11),
+    user_id INT(11),
+    type ENUM('light','medium','heavy'),
+  CONSTRAINT trucks_pk PRIMARY KEY (vehicle_id)
+);
+
+CREATE TABLE motorcycles(
+    vehicle_id INT(11),
+    user_id INT(11),
+    type ENUM('standart','cruiser','sport','touring','scooter','off-road'),
+  CONSTRAINT motorcycles_pk PRIMARY KEY (vehicle_id)
+);
+
+CREATE TABLE posts(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11),
+    vehicle_id INT(11),
+    price INT(7) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+  CONSTRAINT posts_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE questions(
-	id INT(11) NOT NULL AUTO_INCREMENT,
-	description VARCHAR (200),
+    id INT(11) NOT NULL AUTO_INCREMENT,
 	user_id INT(11),
-CONSTRAINT questions_id PRIMARY KEY (id)
+    post_id INT(11),
+    description VARCHAR(200) NOT NULL, 
+  CONSTRAINT questions_pk PRIMARY KEY (id)
 );
 
+CREATE TABLE answers(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11),
+    post_id INT(11),
+    description VARCHAR(200) NOT NULL,
+  CONSTRAINT answers_pk PRIMARY KEY (id)
+);
