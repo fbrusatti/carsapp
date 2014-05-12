608a32be629a4a777dd71d542eb2df944c116bab
Angelo Chaves <angeloechaves@gmail.com>
2014-05-12 18:29:09 -0300

    Uptade App

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -4,6 +4,8 @@ import org.javalite.activejdbc.Base;
 
 import com.unrc.app.models.User;
 import com.unrc.app.models.Vehicle;
+import com.unrc.app.models.Post;
+import com.unrc.app.models.Question;
 
 /**
  * Hello world!
@@ -20,15 +22,29 @@ public class App
         User user = new User();
         user.set("first_name", "Marilyn");
         user.set("last_name", "Monroe");
+        user.set("email","monroe@example.com");
         // user.set("dob", "1935-12-06");
         user.saveIt();
 
-        User.createIt("first_name", "Marcelo", "last_name", "Uva");
+        User.createIt("first_name", "Marcelo", "last_name", "Uva","email","uva@dc.exa.unrc.edu.ar");
 
+        //vehicle stuff
         Vehicle vehicle = Vehicle.create("name", "Honda Accord","model","1999","km","32000");
         vehicle.saveIt();
 
-        user.add(vehicle);
+        //post stuff
+        Post post = Post.create("price","15000","description","hello world");
+        vehicle.add(post);
+        user.add(vehicle); //the vehicle belong to this user
+        user.add(post); //and is posted on this post (redundance)
+
+        //question time!
+        Question question = new Question();
+        question.set("description", "hola?");
+        //should check the user is not asking in self post
+
+        user.add(question); //this user is asking
+        post.add(question); //on this post
 
         Base.close();
     }
