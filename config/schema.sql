6670cc324ce2418ee86771e0f02fd1f14d89ecc5
Angelo Chaves <angeloechaves@gmail.com>
2014-05-13 15:26:11 -0300

    Update schema with default value 'other' for enum columns

--- config/schema.sql
+++ config/schema.sql
@@ -33,7 +33,7 @@ DROP TABLE IF EXISTS cars;
 CREATE TABLE cars(
     vehicle_id INT(11) NOT NULL AUTO_INCREMENT,
     user_id INT(11),
-    type ENUM('sedan','compact','coupe','wagon','sports','other'),
+    type ENUM('sedan','compact','coupe','wagon','sports','other') DEFAULT 'other',
   CONSTRAINT cars_pk PRIMARY KEY (vehicle_id)
 );
 
@@ -42,7 +42,7 @@ DROP TABLE IF EXISTS trucks;
 CREATE TABLE trucks(
     vehicle_id INT(11),
     user_id INT(11),
-    type ENUM('light','medium','heavy','other'),
+    type ENUM('light','medium','heavy','other') DEFAULT 'other',
   CONSTRAINT trucks_pk PRIMARY KEY (vehicle_id)
 );
 
@@ -50,7 +50,7 @@ DROP TABLE IF EXISTS motorcycles;
 CREATE TABLE motorcycles(
     vehicle_id INT(11),
     user_id INT(11),
-    type ENUM('standart','cruiser','sport','touring','scooter','off-road','other'),
+    type ENUM('standart','cruiser','sport','touring','scooter','off-road','other') DEFAULT 'other',
   CONSTRAINT motorcycles_pk PRIMARY KEY (vehicle_id)
 );
 
