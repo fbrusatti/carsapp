d287e17d57d49f5cfe04256fe48c8fb79cc14ca4
Javier Furlan <javifurlan@hotmail.com>
2014-05-14 16:52:34 -0300

    Database funcionando

--- config/schema.sql
+++ config/schema.sql
@@ -1,4 +1,4 @@
--- Integrantes: Arague Ezequiel - Furlan Javier - Martinez Gustavo - Tissera Adrian
+-- Integrantes: Arangue Ezequiel - Furlan Javier - Martinez Gustavo - Tissera Adrian
 drop database carsapp_development;
 create database carsapp_development;
 use carsapp_development;
@@ -12,7 +12,6 @@ CREATE TABLE users(
     -- count_operation INT NOT NULL,
 	CONSTRAINT users_pk PRIMARY KEY (id)
 );
-/*
 
 DROP TABLE IF EXISTS answers; -- Respuesta
 CREATE TABLE answers(
@@ -31,34 +30,6 @@ CREATE TABLE questions(
     id_answers int(11),
   CONSTRAINT question_pk PRIMARY KEY (id),
   CONSTRAINT answers_questions_fk FOREIGN KEY (id_answers) REFERENCES answers(id)
-  on update cascade on delete cascade
-
-);
-
-
-DROP TABLE IF EXISTS posts; -- Publicaciones
-CREATE TABLE posts(
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    description VARCHAR(2000),
-    id_users int(11),
-    id_question  int(11),
-    id_vehicle varchar(6),
-   CONSTRAINT users_posts_fk FOREIGN KEY (id_users) REFERENCES users(id),
-   CONSTRAINT vehicle_post_fk FOREIGN KEY (id_vehicle) REFERENCES vehicles(patent),  
-   CONSTRAINT question_posts_fk FOREIGN KEY (id_question) REFERENCES questions(id),  
-   CONSTRAINT posts_pk PRIMARY KEY (id)
-
-);
-
-DROP TABLE IF EXISTS rates; -- Calificaciones de las publicaciones
-CREATE TABLE rates(
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    points_p int,
-    id_post int not null,
-    id_user int not null,
-  CONSTRAINT rates_pk PRIMARY KEY (id),
-  CONSTRAINT posts_rates_fk foreign key (id_post) REFERENCES posts(id),
-  CONSTRAINT users_rates_fk foreign key (id_user) REFERENCES users(id)
   on update cascade on delete cascade
 
 );
@@ -119,8 +90,8 @@ CREATE TABLE motocicles(
 );
 
 
-DROP TABLE IF EXISTS adresss; -- Direccion
-CREATE TABLE adresss(
+DROP TABLE IF EXISTS adress; -- Direccion
+CREATE TABLE adress(
     direction varchar(20) NOT NULL,
     city varchar(20),
     province varchar(20),
@@ -128,10 +99,38 @@ CREATE TABLE adresss(
     CONSTRAINT adresss_pk PRIMARY KEY (direction)
 );
 
-DROP TABLE IF EXISTS user_adresss; -- tabla que contiene la informacion para referenciar a usuario y direccion
-CREATE TABLE user_adresss(
+DROP TABLE IF EXISTS user_adress; -- tabla que contiene la informacion para referenciar a usuario y direccion
+CREATE TABLE user_adress(
     direction_adress varchar(20) NOT NULL,
     id_user int(11),
     CONSTRAINT adress_user_u_fk foreign key (direction_adress) REFERENCES adress(direction),
     CONSTRAINT adress_user_d_fk foreign key (id_user) REFERENCES users(id)
-);*/
+
+);
+
+DROP TABLE IF EXISTS posts; -- Publicaciones
+CREATE TABLE posts(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    description VARCHAR(2000),
+    id_users int(11),
+    id_question  int(11),
+    id_vehicle varchar(6),
+   CONSTRAINT users_posts_fk FOREIGN KEY (id_users) REFERENCES users(id),
+   CONSTRAINT vehicle_post_fk FOREIGN KEY (id_vehicle) REFERENCES vehicles(patent),  
+   CONSTRAINT question_posts_fk FOREIGN KEY (id_question) REFERENCES questions(id),  
+   CONSTRAINT posts_pk PRIMARY KEY (id)
+
+);
+
+DROP TABLE IF EXISTS rates; -- Calificaciones de las publicaciones
+CREATE TABLE rates(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    points_p int,
+    id_post int not null,
+    id_user int not null,
+  CONSTRAINT rates_pk PRIMARY KEY (id),
+  CONSTRAINT posts_rates_fk foreign key (id_post) REFERENCES posts(id),
+  CONSTRAINT users_rates_fk foreign key (id_user) REFERENCES users(id)
+  on update cascade on delete cascade
+
+);
