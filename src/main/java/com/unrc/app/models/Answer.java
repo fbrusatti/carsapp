f616674c749702521fd57772b24eae8cf11bf035
LfLanzoni <lflanzoni@gmail.com>
2014-05-16 08:58:06 -0300

    Creacion Clase Answer

---
+++ src/main/java/com/unrc/app/models/Answer.java
@@ -0,0 +1,12 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+public class Answer extends Model {
+  static {
+      validatePresenceOf("id_question","id_user", "text");
+  }
+  
+  public String getText(){
+	  return (this.getString("text"));
+  }
