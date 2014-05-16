ae008a91021900d0d400124a9c4767b71ab75180
LfLanzoni <lflanzoni@gmail.com>
2014-05-16 08:53:59 -0300

    Modificacion Clase User

--- src/main/java/com/unrc/app/models/User.java
+++ src/main/java/com/unrc/app/models/User.java
@@ -3,7 +3,39 @@ package com.unrc.app.models;
 import org.javalite.activejdbc.Model;
 
 public class User extends Model {
-  static {
-      validatePresenceOf("first_name", "last_name");
-  }
+
+	static {
+      validatePresenceOf("first_name", "last_name","email");
+  	}
+	
+	//get First Name
+	public string getFirstName(){
+		return (this.getString("first_name"));
+	}
+	
+	//get LastName
+	public string getLastName (){
+		return (this.getString("last_name"));
+	}
+	
+	//get email
+	public string getEmail(){
+		return (this.getString("email"));
+	}
+	
+	//find user
+	public User findUser (String email){
+		return User.findFirst("email = ?",email);
+	}
+	
+	//delete user javalite.io/batch_operations
+	public boolean deleteUser(String email){
+		return User.findFirst("email = ?").delete();
+	}
+	
+	
+	}
+	
+	
+	
 }
