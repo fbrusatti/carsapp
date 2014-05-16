7f470d3ea82885d1cfb3b63634410c288fa25b43
Gustavo <gustavo@gustavo-HP-G42-Notebook-PC.(none)>
2014-05-16 17:16:33 -0300

    App principal

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -17,16 +17,11 @@ public class App
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "root");
 
         User user = new User();
-        user.set("first_name", "Marilyn");
-        user.set("last_name", "Monroe");
-        user.set("email", "gustavomartinez@gmail.com");
-        user.saveIt();
-
-        User.createIt("first_name", "Marcelo", "last_name", "Uva", "email", "martinez@gmail.com");
         String nombre="gustavo";
         String apellido="martinez";
         String mail="mfwebdesign@gmail.com";
         User.createUser(nombre, apellido, mail);
+
 
         Base.close();
     }
