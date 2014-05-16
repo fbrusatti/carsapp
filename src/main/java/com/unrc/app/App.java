133cf2569fd542e931ce0146a53c85ffe2644481
Maximiliano <maxi@huayra.(none)>
2014-05-16 17:07:29 -0300

    Update App with car add example

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -5,6 +5,7 @@ import org.javalite.activejdbc.Base;
 import com.unrc.app.models.User;
 import com.unrc.app.models.Vehicle;
 import com.unrc.app.models.Post;
+import com.unrc.app.models.Car;
 import com.unrc.app.models.Question;
 
 /**
@@ -15,7 +16,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello cruel World!" );
+        System.out.println( "Hello cruel world!" );
 
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "");
 
@@ -23,14 +24,19 @@ public class App
         user.set("first_name", "Marilyn");
         user.set("last_name", "Monroe");
         user.set("email","monroe@example.com");
+        user.set("is_admin","0");  //True 
         // user.set("dob", "1935-12-06");
         user.saveIt();
 
-        User.createIt("first_name", "Marcelo", "last_name", "Uva","email","uva@dc.exa.unrc.edu.ar");
+        user.createUser("Marcelo","Uva","uva@dc.exa.unrc.edu.ar");
 
         //vehicle stuff
         Vehicle vehicle = Vehicle.create("name", "Honda Accord","model","1999","km","32000");
         vehicle.saveIt();
+
+        //car stuff
+        Car car = Car.create ("type","sedan"); //there are errors of validation if created empty. That means that the field type is not "other" from default
+        vehicle.add(car);
 
         //post stuff
         Post post = Post.create("price","15000","description","hello world");
