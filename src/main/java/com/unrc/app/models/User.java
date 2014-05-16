bf6f9842c95f1a2bce914bf74dbadb6b51536a80
Maximiliano <maxi@huayra.(none)>
2014-05-16 17:08:38 -0300

    Add user with createUser method

--- src/main/java/com/unrc/app/models/User.java
+++ src/main/java/com/unrc/app/models/User.java
@@ -6,4 +6,11 @@ public class User extends Model {
   static {
       validatePresenceOf("first_name", "last_name", "email");
   }
+
+/* Before the invocation of the method, should check if is the admin who create the user*/
+  public void createUser (String fn, String ln, String email){
+  	if (getInteger("is_admin")==1){
+  		User.createIt("first_name", fn, "last_name", ln,"email",email,"is_admin","0");
+  	}
+  }
 }
