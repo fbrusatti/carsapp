adaf0590952ed79aa3542859f2a221a2dadced91
castagneris <ncastagneris@gmai.com>
2014-05-18 21:15:50 -0300

    actualizacion

--- src/main/java/com/unrc/app/models/Vehicle.java
+++ src/main/java/com/unrc/app/models/Vehicle.java
@@ -4,12 +4,7 @@ import org.javalite.activejdbc.Model;
 
 public class Vehicle extends Model {
   static {
-      validatePresenceOf("id_user","model", "color","km","mark","year");
-  }
-  
-  //Id get vehicle owner
-  public Int getIdUser (){
-	  return (this.getString("id_user")); 
+      validatePresenceOf("model", "color","km","mark","year");
   }
   
   //get model
@@ -23,19 +18,16 @@ public class Vehicle extends Model {
   }
   
   //get km
-  public int getKm(){
-	  return (this.getInt("km"));
+  public Integer getKm(){
+	  return (this.getInteger("km"));
   }
   //get mark
   public String getMark(){
-	  return (this.getInt("mark"));
+	  return (this.getString("mark"));
   } 
   
   //get year
-  public int getYear(){
-	  return (this.getInt("year"));
+  public Integer getYear(){
+	  return (this.getInteger("year"));
   }
-  
-
-  
 }
