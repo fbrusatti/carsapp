26a9fbf15a691609264d0312d1e66e9f94faab51
castagneris <ncastagneris@gmai.com>
2014-05-18 21:17:51 -0300

    actualizacion

---
+++ src/main/java/com/unrc/app/models/City.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.unrc.app.models;
+import org.javalite.activejdbc.Model;
+/**
+ *
+ * @author castagneris
+ */
+public class City extends Model {
+    static {
+      validatePresenceOf("name");
+  	}
+	
+	//get Name
+	public String getName(){
+		return (this.getString("name"));
+	}
+    
+}
