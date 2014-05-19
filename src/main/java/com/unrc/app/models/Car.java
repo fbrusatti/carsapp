bea4ba4bdf967f161bbb79716f71b9e26ec61291
castagneris <ncastagneris@gmai.com>
2014-05-18 21:12:56 -0300

    actualizacion

--- src/main/java/com/unrc/app/models/Car.java
+++ src/main/java/com/unrc/app/models/Car.java
@@ -4,22 +4,12 @@ import org.javalite.activejdbc.Model;
 
 public class Car extends Model {
   static {
-	  validatePresenceOf("id_vehicle","id_user","doors","version","transmission","direction");	  
+	  validatePresenceOf("doors","version","transmission","direction");	  
   }
-  
-  //Id get vehicle owner
-  public String getIdVehicle (){
-	  return (this.getString("id_vehicle")); 
-  }
-  
-  //get user
-  public String getIdUser(){
-	  return (this.getString("id_user"));
-  }
-  
+
   //get doors
-  public String getDoors(){
-	  return (this.getString("doors"));
+  public Integer getDoors(){
+	  return (this.getInteger("doors"));
   }
   
   //get version
