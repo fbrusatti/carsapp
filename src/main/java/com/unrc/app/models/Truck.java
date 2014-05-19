aa3ed6a86368b93ef3775c616d56e6a34ad8c662
castagneris <ncastagneris@gmai.com>
2014-05-18 21:15:07 -0300

    actualizacion

--- src/main/java/com/unrc/app/models/Truck.java
+++ src/main/java/com/unrc/app/models/Truck.java
@@ -4,20 +4,9 @@ import org.javalite.activejdbc.Model;
 
 public class Truck extends Model {
   static {
-	  validatePresenceOf("id_vehicle","id_user","brake_System","direction","capacity");	  
+	  validatePresenceOf("brake_System","direction","capacity");	  
   }
  
-	
-  //Id get vehicle owner
-  public String getIdVehicle (){
-	  return (this.getString("id_vehicle")); 
-  }
-  
-  //get id_user
-  public String getIdUser(){
-	  return (this.getString("id_user"));
-  }
-  
   //get brake_system
   public String getbrakeSystem(){
 	  return (this.getString("brake_system"));
