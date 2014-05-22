5042e8e849fb845f24c624f081838888a8fc6a3e
Javier Furlan <javifurlan@hotmail.com>
2014-05-22 13:12:51 -0300

    AddressTest up-to-date

--- src/test/java/com/unrc/app/AddressTest.java
+++ src/test/java/com/unrc/app/AddressTest.java
@@ -1,7 +1,7 @@
 package com.unrc.app;
 
-import com.unrc.app.models.User;
 import com.unrc.app.models.Address;
+import com.unrc.app.models.User;
 
 import org.javalite.activejdbc.Base;
 import org.junit.After;
@@ -32,8 +32,8 @@ public class AddressTest{
 
         the(address).shouldNotBe("valid");
         the(address.errors().get("street")).shouldBeEqual("value is missing");
-        address.set("street", "Estrada");
-        address.set("num", 2030);
+        address.set("street", "Lincoln");
+        address.set("num", 874);
 
         // Everything is good:
         the(address).shouldBe("valid");
@@ -41,42 +41,49 @@ public class AddressTest{
 
     @Test
     public void shouldValidateCreateAddress(){
-        Address address = Address.createAddress("Estrada", 2030);
+        User u = User.createUser("Didier","Drogba","tito_drogba@hotmail.com");
+        Address address = Address.createAddress("Lincoln", 874,u);
         the(address).shouldBe("valid");
         the(address).shouldNotBeNull();
-        the(address).shouldContain("Estrada");
-        the(address).shouldContain(2030);
+        the(address).shouldContain("Lincoln");
+        the(address).shouldContain(874);
     }
 
     //Creo un Address, luego la busco y me fijo si son el mismo Address
     //También creo un Address distinto, y 
     @Test
     public void shouldValidateFindByAddress(){
-        Address a = Address.createAddress("Estrada", 2030);
-        Address a3 = Address.createAddress("Rioja", 1023);
-        Address a2 = Address.findByAddress("Estrada", 2030);
+        User u = User.createUser("Didier","Drogba","tito_drogba@hotmail.com");
+        Address a = Address.createAddress("Lincoln", 874,u);
+        Address a3 = Address.createAddress("Rioja", 1023,u);
+        Address a2 = Address.findByAddress("Lincoln", 874);
         the(a2.getString("street")).shouldBeEqual(a.getString("street"));  
         the(a2.getInteger("num")).shouldBeEqual(a.getInteger("num"));  
         the(a3.getString("street")).shouldNotBeEqual(a.getString("street"));   
         the(a3.getInteger("num")).shouldNotBeEqual(a.getInteger("num"));   
     }
 
-    //verifico si un usuario(creado anteriormente) existe y luego busco un usuario inexistente
+    //verifico si una direccion(creada anteriormente) existe y luego busco una direccion inexistente
     @Test
     public void shouldValidateExistsAddress(){
-        Address a = Address.createAddress("Estrada", 2030);
+        User u = User.createUser("Didier","Drogba","tito_drogba@hotmail.com");
+        Address a = Address.createAddress("Lincoln", 874,u);
         the(Address.existAddress(a.getString("street"),a.getInteger("num"))).shouldBeTrue();
         the(Address.existAddress("Parana", 1024)).shouldBeFalse();
     }
 
+    //Creo un usuario y una direccion asociada a este usuario("Didier Drogba -> Lincoln 874").
+    //Intento eliminar Lincoln 874, lo cual NO se puede (tiene un usuario asociado)
+    //Asocio una nueva direccion al usuario (Didier Drogba -> San Martin 1654)
+    //Elimino la relacion entre Didier Drogba -> San Martin 1654
+    //Intento eliminar San Martin 1654, lo cual SI se puede (NO tiene ningun usuario asociado)
     @Test
-    public void shouldValidateDeleteAddress(){
-      Address a = Address.createAddress("Estrada", 2030);
-      the(Address.deleteAddress("Estrada", 2030)).shouldBeTrue();
-      the(Address.deleteAddress("Another Street", 4567)).shouldBeFalse();
-      the(Address.deleteAddress("Another Street", 2030)).shouldBeFalse();
-      the(Address.deleteAddress("Estrada", 4567)).shouldBeFalse();
-
-      
+    public void shouldValidateDeleteAddress(){        
+      User u = User.createUser("Didier","Drogba","tito_drogba@hotmail.com");
+      Address a = Address.createAddress("Lincoln", 874,u);
+      Address a2 = Address.createAddress("San Martin", 1654,u);
+      User.deleteUserAddress("San Martin",1654,"tito_drogba@hotmail.com"); //elimino la relacion San Martin 1654 -> Didier Drogba
+      the(Address.deleteAddress("Lincoln", 874)).shouldBeFalse(); //no la puedo eliminar porque tiene un usuario asociado
+      the(Address.deleteAddress("San Martin", 1654)).shouldBeTrue(); //se deberia poder eliminar(nada asociado a ella)
     } 
 }
