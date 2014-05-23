d335be40931a4c89bd043f08bd2520c101c0d5d6
Angelo Chaves <angeloechaves@gmail.com>
2014-05-22 23:49:19 -0300

    Uptade App - Add Spark resources

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -23,51 +23,8 @@ public class App
         //     return "Hello World!";
         // });
 
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "");
-
-        User user = new User();
-        user.set("first_name", "Marilyn");
-        user.set("last_name", "Monroe");
-        user.set("email","monroe@example.com");
-        user.set("is_admin","1");  //True 
-        // user.set("dob", "1935-12-06");
-        user.saveIt();
-        // get("/users",(request, response) -> {
-        //     return user;
-        // });
-
-        user.createUser("Marcelo","Uva","uva@dc.exa.unrc.edu.ar");
-
-        //vehicle stuff
-        Vehicle vehicle = Vehicle.create("name", "Honda Accord","model","1999","km","32000");
-        vehicle.saveIt();
-
-        //car stuff
-        Car car = Car.create ("type","sedan"); //there are errors of validation if created empty. That means that the field type is not "other" from default
-        vehicle.add(car);
-
-        //post stuff
-        Post post = Post.create("price","15000","description","hello world");
-        vehicle.add(post);
-        user.add(vehicle); //the vehicle belong to this user
-        user.add(post); //and is posted on this post (redundance)
-
-        //question time!
-        Question question = new Question();
-        question.set("description", "hola?");
-        //should check the user is not asking in self post
-
-        user.add(question); //this user is asking
-        post.add(question); //on this post
-
-
-        //Spark section----------
-
         before((request, response) -> {
             Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "");
-        });
-        after((request, response) -> {
-            Base.close();    
         });
 
         //Users url
@@ -88,6 +45,8 @@ public class App
             return "Post: " + p.toString();
         });
 
-        Base.close();
+        after((request, response) -> {
+            Base.close();    
+        });
     }
 }
