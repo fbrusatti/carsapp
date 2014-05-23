d648b41eeda302ad85a741aef2ffdeb17ba5d2b5
Angelo Chaves <angeloechaves@gmail.com>
2014-05-22 23:41:35 -0300

    Update schema - Add insterts

--- config/schema.sql
+++ config/schema.sql
@@ -6,23 +6,23 @@ CREATE TABLE users(
     email VARCHAR(60) UNIQUE,
     first_name VARCHAR(56),
     last_name VARCHAR(56),
-	address_id INT(11),
     is_admin BOOLEAN,
   CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS addresses;
 CREATE TABLE addresses(
-	id INT (11) NOT NULL AUTO_INCREMENT,
-	street VARCHAR(120),
-	address_number INT(5),
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11),
+    street VARCHAR(120),
+    address_number INT(5),
   CONSTRAINT addresses_pk PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS vehicles;
 CREATE TABLE vehicles(
-	id INT(11) NOT NULL AUTO_INCREMENT,
-	user_id INT(11),
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11),
     name VARCHAR(56),
     model INT(4),
     km INT(7),
@@ -64,7 +64,7 @@ CREATE TABLE posts(
 DROP TABLE IF EXISTS questions;
 CREATE TABLE questions(
     id INT(11) NOT NULL AUTO_INCREMENT,
-	user_id INT(11),
+    user_id INT(11),
     post_id INT(11),
     description VARCHAR(200) NOT NULL, 
   CONSTRAINT questions_pk PRIMARY KEY (id)
@@ -78,3 +78,12 @@ CREATE TABLE answers(
     description VARCHAR(200) NOT NULL,
   CONSTRAINT answers_pk PRIMARY KEY (id)
 );
+
+/* Inserting values */
+
+INSERT INTO users (first_name,last_name,email,is_admin) VALUES("Marilyn","Monroe","monroe@example.com",1);
+INSERT INTO users (first_name,last_name,email,is_admin) VALUES("Marcelo","Uva","uva@example.com",0); 
+INSERT INTO vehicles (user_id,name,model,km) VALUES(1,"Honda Accord","1999","32000");
+INSERT INTO cars (vehicle_id,type) VALUES(1,"sedan");
+INSERT INTO posts (user_id,vehicle_id,price,description) VALUES(1,1,"15000","hello world");
+INSERT INTO questions (user_id,post_id,description) VALUES (1,1,"hello question?");
