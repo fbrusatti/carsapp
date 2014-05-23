b9596e70f7a16b06e18f5d341e94800ef8c27411
castagneris <ncastagneris@gmai.com>
2014-05-23 00:12:14 -0300

    Documentacion

--- src/test/java/com/unrc/app/VehicleTest.java
+++ src/test/java/com/unrc/app/VehicleTest.java
@@ -1,12 +1,20 @@
 package com.unrc.app;
 
-import com.unrc.app.models.User;
+import com.unrc.app.models.Vehicle;
+
+import org.javalite.activejdbc.Base;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.javalite.test.jspec.JSpec.the;
+import static org.junit.Assert.assertEquals;
+
 
 public class VehicleTest{
 	
 	@Before
     public void before(){
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_test", "root", "");
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_test", "root", "root");
         System.out.println("VehicleTest setup");
         Base.openTransaction();
     }
@@ -21,15 +29,17 @@ public class VehicleTest{
     @Test
     public void shouldValidateMandatoryFields(){
         Vehicle vehicle = new Vehicle();
-        User user = new User();
 
-        the(Vehicle).shouldNotBe("valid");
-        the(Vehicle.errors().get("id_user")).shouldBeEqual("value is missing");
-        the(user.errors().get("last_name")).shouldBeEqual("value is missing");
+        the(vehicle).shouldNotBe("valid");
+        the(vehicle.errors().get("model")).shouldBeEqual("value is missing");
+        the(vehicle.errors().get("color")).shouldBeEqual("value is missing");
+        the(vehicle.errors().get("km")).shouldBeEqual("value is missing");
+        the(vehicle.errors().get("mark")).shouldBeEqual("value is missing");
+        the(vehicle.errors().get("year")).shouldBeEqual("value is missing");
 
-        user.set("first_name", "John", "last_name", "Doe", "email", "example@email.com");
+        vehicle.set("model", "Gol", "color","black", "km","0", "mark", "Volkswagen","year","2014");
 
         // Everything is good:
-        the(user).shouldBe("valid");
+        the(vehicle).shouldBe("valid");
     }
 }
