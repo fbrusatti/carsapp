20e48970c0ff89b9bf37e45fa65fdd7a3675f0f8
Angelo Chaves <angeloechaves@gmail.com>
2014-05-23 12:54:12 -0300

    Uptade App - now list users in /users

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -9,6 +9,7 @@ import com.unrc.app.models.Vehicle;
 import com.unrc.app.models.Post;
 import com.unrc.app.models.Car;
 import com.unrc.app.models.Question;
+import com.unrc.app.models.Answer;
 
 /**
  * Hello world!
@@ -25,24 +26,52 @@ public class App
 
         before((request, response) -> {
             Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "");
-        });
+        }); //could redirect to a "home"
 
-        //Users url
-        get("/user/:id", (request, response) -> {
+        //List of users
+        get("/users", (request,response) -> {
+            List<User> userList = User.findAll();
+            String list = new String();
+            for (User u: userList) {
+                list = list+u.getString("id")+" "+u.getString("first_name")+" "+u.getString("last_name")+"\n";
+            }
+            return list;
+        });
+        
+        //Show Users 
+        get("/users/:id", (request, response) -> {
             User u = User.findById(Integer.parseInt(request.params(":id")));
-            return "User: " + u.toString();
+            String name = u.getString("first_name") +" "+ u.getString("last_name");
+            String email = u.getString("email");
+            return "User: "+name+"\n"+"Email: "+email+"\n";
         });
 
-        //Vehicles url
-        get("/vehicle/:id", (request, response) -> {
+        //Show Vehicles 
+        get("/vehicles/:id", (request, response) -> {
             Vehicle v = Vehicle.findById(Integer.parseInt(request.params(":id")));
-            return "Vehicle: " + v.toString();
+            String vehicleName = v.getString("name") +" "+ v.getString("model");
+            String km = v.getString("km");
+            User u1 = User.findById(v.getInteger("user_id"));
+            String userName = u1.getString("first_name");
+            return "Vehicle: " + vehicleName+"\n"+"Belongs to: "+userName;
         });
 
-        //Posts url
-        get("/post/:id", (request, response) -> {
+        //Show Posts
+        get("/posts/:id", (request, response) -> {
             Post p = Post.findById(Integer.parseInt(request.params(":id")));
             return "Post: " + p.toString();
+        });
+
+        //Show Questions 
+        get("/questions/:id", (request, response) -> {
+            Question q = Question.findById(Integer.parseInt(request.params(":id")));
+            return "Question: " + q.toString();
+        });
+
+        //Show Answers
+        get("/answers/:id", (request, response) -> {
+            Answer a = Answer.findById(Integer.parseInt(request.params(":id")));
+            return "Answer: " + a.toString();
         });
 
         after((request, response) -> {
