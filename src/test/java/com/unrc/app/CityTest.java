569610f6c1efb61f0242ea386a3784b464e01731
castagneris <ncastagneris@gmai.com>
2014-05-23 00:05:40 -0300

    Actualizacion

---
+++ src/test/java/com/unrc/app/CityTest.java
@@ -0,0 +1,42 @@
+package com.unrc.app;
+
+import com.unrc.app.models.City;
+
+import org.javalite.activejdbc.Base;
+import static org.javalite.test.jspec.JSpec.the;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CityTest {
+    
+    @Before
+    public void before(){
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_test", "root", "root");
+        System.out.println("CityTest setup");
+        Base.openTransaction();
+    }
+
+    @After
+    public void after(){
+        System.out.println("CityTest tearDown");
+        Base.rollbackTransaction();
+        Base.close();
+    }
+
+    @Test
+    public void shouldValidateMandatoryFields(){
+        City city = new City();
+
+        the(city).shouldNotBe("valid");
+        the(city.errors().get("country")).shouldBeEqual("value is missing");
+        the(city.errors().get("state")).shouldBeEqual("value is missing");
+        the(city.errors().get("name")).shouldBeEqual("value is missing");
+        
+        
+        city.set("country","Argentina","state","Cordoba","name","La Carlota");
+
+        the(city).shouldBe("valid");
+    } 
+}
