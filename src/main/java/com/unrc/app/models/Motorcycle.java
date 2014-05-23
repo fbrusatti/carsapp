2f67ebfc5641c00072c5c9b0f4b52500b520c33c
castagneris <ncastagneris@gmai.com>
2014-05-23 19:20:22 -0300

    cambio de nombre

---
+++ src/main/java/com/unrc/app/models/Motorcycle.java
@@ -0,0 +1,29 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+public class Motorcycle extends Model {
+  static {
+	  validatePresenceOf("type","type_motor","boot_system","displacement");	  
+  }
+  
+  //get type
+  public String getType(){
+	  return (this.getString("type"));
+  }
+  
+  //get type_motor
+  public String getTypeMotor(){
+	  return (this.getString("type_motor"));
+  }
+  //get boot_sistem
+  public String getBootSitem(){
+	  return (this.getString("boot_system"));
+  } 
+  
+  //get displacement
+  public String getDisplacement(){
+	  return (this.getString("displacement"));
+  }
+
+}
