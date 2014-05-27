0f238bb52c9849d2c32bc64c805b88f42c9b044d
Maximiliano <maximiliano.e.chaves@gmail.com>
2014-05-27 19:58:36 -0300

    Add forms in /users/new

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -4,6 +4,11 @@ import org.javalite.activejdbc.*;
 import static spark.Spark.*;
 import java.util.List;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import com.unrc.app.models.User;
 import com.unrc.app.models.Vehicle;
 import com.unrc.app.models.Post;
@@ -20,13 +25,44 @@ public class App
     public static void main( String[] args )
     {
         //System.out.println( "Hello cruel world!" );
-        // get("/hello",(request, response) -> {
-        //     return "Hello World!";
-        // });
+        try {                           //code to open browser in hello url. problem, it loads without the content of spark
+            URI uri = new URI("http://localhost:4567/users/new");
+            Desktop desktop = null;
+            if (Desktop.isDesktopSupported()) {
+                desktop = Desktop.getDesktop();
+            }
+            if (desktop != null){
+                desktop.browse(uri);
+            }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } catch (URISyntaxException use) {
+                use.printStackTrace();
+            }
+
+        get("/hello",(request, response) -> {
+            return "Hello World!";
+        });
 
         before((request, response) -> {
             Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "");
         }); //could redirect to a "home"
+
+        
+
+        get("/users/new" , (request, response) ->{
+            response.type("text/html");
+            return "<html> \n <head> \n <title> \n Create user \n </title> \n </head> \n <body> \n <form action=\"/users\" method=\"post\"> \n name \n <input type= \"text\" name=\"name\"> \n email \n <input type=\"text\" name= \"email\"> \n <input type=\"submit\" value= \"Guardar\"> \n </form> \n </body>\n </html>";
+        });
+
+        post ("/users",(request, response) ->{ 
+            String name = request.queryParams("name");
+            String email = request.queryParams("email");
+            User.createIt("first_name", name, "last_name", "lastname","email",email,"is_admin","0"); //acá iria nuestro metodo de creacion
+            response.redirect("/users");
+            return "success"; //
+        });
+
 
         //List of users
         get("/users", (request,response) -> {
