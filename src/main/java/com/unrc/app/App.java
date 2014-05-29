92b26af4572dc898d16d6d5b917606ede1087004
Javier Furlan <javifurlan@hotmail.com>
2014-05-28 21:25:50 -0300

    Added get's

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -4,6 +4,7 @@ import org.javalite.activejdbc.Base;
 
 import com.unrc.app.models.*;
 
+import java.util.List;
 
 import static spark.Spark.*;
 /**
@@ -17,13 +18,6 @@ public class App
         System.out.println( "Hello cruel World!" );
 
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "root");
-        /*
-        User user = new User();
-        String nombre="gustavo";
-        String apellido="martinez";
-        String mail="mfwebdesign@gmail.com";
-        User.createUser(nombre, apellido, mail);
-        */
         User user = User.createUser ("gustavo", "martinez", "mfwebdesign@gmail.com");
         User user2 = User.createUser("adrian", "tissera", "adriantissera@gmail.com");
         Base.close();
@@ -58,7 +52,7 @@ public class App
         Base.close();
 
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "root");
-        Vehicle vehicle3 = Vehicle.createVehicle("asd456","nija","kawasaki", User.findByEmail("mfwebdesign@gmail.com"));
+        Vehicle vehicle3 = Vehicle.createVehicle("asd456","ninja","kawasaki", User.findByEmail("mfwebdesign@gmail.com"));
         Base.close();
 
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "root");
@@ -88,6 +82,104 @@ public class App
 
         get("/hello", (request, response) -> {
             return "Hello world";
+        });
+
+        before((request, response) -> {
+            Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "root");
+        }); 
+
+        //Lista todo los users
+        get("/users",(request,response)->{
+            List<User> userList = User.findAll();
+            String list = new String();
+            for (User u: userList) {
+                list = list+u.getString("id")+" "+u.getString("first_name")+" "+u.getString("last_name")+" "+u.getString("email")+"<br>";
+            }
+            return list;
+        });
+
+        //Lista un usuario particular
+        get("/users/:id",(request,response)->{
+            User userRequested = User.findById(request.params(":id"));
+            if(userRequested!=null){
+                String name = userRequested.getString("first_name")+" "+userRequested.getString("last_name");
+                String email = userRequested.getString("email");
+                return "Nombre: "+name+" Email: "+email;
+            }
+            else{
+                return "Usuario no encontrado!";
+            }            
+        });
+
+        get("/vehicle",(request,response)->{
+            List<Vehicle> vehiclesList = Vehicle.findAll();
+            String list = new String();
+            for(Vehicle v: vehiclesList){
+                list = list+v.getString("patent")+" "+v.getString("model")+" "+v.getString("brand")+"<br>";
+            }
+            return list;
+        });
+
+        //Lista un vehiculo particular
+        get("/vehicle/:patent",(request,response)->{
+            Vehicle vehicleRequested = Vehicle.findByPatent(request.params(":patent"));
+            if(vehicleRequested!=null){
+                String patente = vehicleRequested.getString("patent");
+                String marca = vehicleRequested.getString("brand");
+                String modelo = vehicleRequested.getString("model");
+                return "Chapa patente: "+patente+". "+"Marca: "+marca+". "+"Modelo: "+modelo;
+            }
+            else{
+                return "Vehiculo no encontrado!";
+            }            
+        });
+
+        get("/post",(request,response)->{
+            List<Post> postsList = Post.findAll();
+            String list = new String();
+            for(Post p: postsList){
+                list = list+"Titulo: "+p.getString("title")+". "+"Descripcion: "+" "+p.getString("description")+"<br>";
+            }
+            return list;
+        });        
+
+        //Lista un post particular
+        get("/post/:id",(request,response)->{
+            Post postRequested = Post.findById(request.params(":id"));
+            if(postRequested!=null){
+                String titulo = postRequested.getString("title");
+                String descripcion = postRequested.getString("description");
+                return "Titulo: "+titulo+". "+"Descripcion: "+descripcion;
+            }
+            else{
+                return "Post no encontrado!";
+            }            
+        });    
+
+        get("/question",(request,response)->{
+            List<Question> questionsList = Question.findAll();
+            String list = new String();
+            for(Question q: questionsList){
+                list = list+"Pregunta: "+q.getString("description")+". "+"<br>";
+            }
+            return list;
+        });    
+
+        //Lista una pregunta particular
+        get("/question/:id",(request,response)->{
+            Question questionRequested = Question.findByQuestion(request.params(":id"));
+            if(questionRequested!=null){
+                String pregunta = questionRequested.getString("description");
+                return "Descripcion: "+pregunta+".";
+            }
+            else{
+                return "Pregunta no encontrado!";
+            }            
+        });    
+
+
+        after((request, response) -> {
+            Base.close();    
         });
 
         /*get(new Route("/hello") {
