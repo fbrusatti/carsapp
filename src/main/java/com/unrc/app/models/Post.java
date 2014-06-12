c9cf983639f378e0ede5ea0103c81f93132a4149
carser <ascardozo830@gmail.com>
2014-06-12 15:47:03 -0300

    Update Post Model

--- src/main/java/com/unrc/app/models/Post.java
+++ src/main/java/com/unrc/app/models/Post.java
@@ -4,6 +4,24 @@ import org.javalite.activejdbc.Model;
 
 public class Post extends Model {
   static {
-	validatePresenceOf("price");
+	validatePresenceOf("price", "description", "user_id", "vehicle_id");
+  }
+
+   public Integer price() {
+    return this.getInteger("price");
+  }
+
+  public String description() {
+    return this.getString("description");
+  }
+
+  public User user() {
+    User u = User.findById(getInteger("user_id"));
+    return u;
+  }
+
+  public Vehicle vehicle() {
+    Vehicle v = Vehicle.findById(getInteger("vehicle_id"));
+    return v;
   }
 }
