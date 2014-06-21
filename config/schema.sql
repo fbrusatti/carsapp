9ea77a18fab9c38c925ddc020f623792f232cbb3
carser <ascardozo830@gmail.com>
2014-06-21 11:40:44 -0300

    update schema

--- config/schema.sql
+++ config/schema.sql
@@ -88,4 +88,4 @@ INSERT INTO vehicles (user_id,name,model,km) VALUES(1,"Honda Accord","1999","320
 INSERT INTO cars (vehicle_id,type) VALUES(1,"sedan");
 INSERT INTO posts (user_id,vehicle_id,price,description) VALUES(1,1,"15000","hello world");
 INSERT INTO questions (user_id,post_id,description) VALUES (2,1,"hello question?");
-INSERT INTO answers (user_id,post_id,description) VALUES (1,1,"bye answer");
+INSERT INTO answers (question_id,user_id,post_id,description) VALUES (1,1,1,"bye answer");
