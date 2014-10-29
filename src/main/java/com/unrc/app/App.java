d1a2c63643b073c2ebd335500a15331a65d951fb
andruvetto <andruvetto@gmail.com>
2014-10-29 16:11:13 -0300

    [Finishes #81594220] SearchController

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -9,26 +9,10 @@ import com.unrc.app.models.*;
 import com.unrc.app.controllers.*;
 import org.javalite.activejdbc.Base;
 
-import spark.Spark.*;
 import spark.Session;
 import spark.ModelAndView;
 import static spark.Spark.*;
 
-import org.elasticsearch.node.*;
-import static org.elasticsearch.node.NodeBuilder.*;
-import org.elasticsearch.client.*;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchRequestBuilder;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHitField;
-import org.elasticsearch.index.query.QueryBuilders.*;
-import org.elasticsearch.index.query.*;
-import org.elasticsearch.search.*;
-import org.elasticsearch.common.settings.*;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 
 
 
@@ -90,96 +74,23 @@ public class App {
             return null;
         });
         
-        /**
+         /**
          * Getting search
          */
         get("/search", (request, response) -> {
-            Session session = request.session(false);
-            boolean existSession = false;
-            if (session != null) existSession = true;
-            Map<String,Object> attributes = new HashMap<String,Object>();
-            attributes.put("existSession", existSession);
-            return new ModelAndView(attributes,"search.mustache");
+            SearchController searchController = new SearchController();
+            return searchController.getSearchView(request);
             },
             new MustacheTemplateEngine()
         );
         
-        /**
+                /**
          * Realizing a search
          */
         post("/search", (request, response) -> {
-           
-            Client client = new TransportClient()
-        					.addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
-
-            ClusterHealthResponse health = client.admin()
-            								.cluster()
-            								.prepareHealth()
-            								.setWaitForGreenStatus()
-            								.execute()
-            								.actionGet();
-
-            SearchResponse resp = new SearchResponse();
-            SearchHit[] docs;
-            Map<String,Object> attributes = new HashMap<String,Object>();
-
-            //The search was executed through users.
-            if (request.queryParams("type").charAt(0)=='2') {
-                resp = client.prepareSearch("users")
-                        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                        .setQuery(QueryBuilders.matchQuery("name",request.queryParams("name"))) 
-                        .setFrom(0).setSize(60).setExplain(true)
-                        .execute()
-                        .actionGet();
+            SearchController searchController = new SearchController();
+            return searchController.postSearchView(request);
             
-            	List<Map<String,Object>> usersFounded = new LinkedList<Map<String,Object>>();
-            	docs = resp.getHits().getHits();
-            	for (SearchHit hit : docs) {
-					Map<String,Object> result = hit.getSource(); 
-					String userId = hit.getId();
-					result.put("id",userId);
-					usersFounded.add(result);
-				}
-
-				attributes.put("searchExecuted",true);
-				if (!usersFounded.isEmpty()) { 
-					attributes.put("results",true); 
-					attributes.put("userSearchResult",usersFounded);
-				}
-            }
-
-            //The search was executed through posts.
-            if (request.queryParams("type").charAt(0)=='1') {
-                resp = client.prepareSearch("posts")
-                        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                        .setQuery(QueryBuilders.matchQuery("title",request.queryParams("dates")))   
-                        .setFrom(0).setSize(60).setExplain(true)
-                        .execute()
-                        .actionGet();
-
-            	List<Map<String,Object>> postsFounded = new LinkedList<Map<String,Object>>();
-            	docs = resp.getHits().getHits();
-            	for (SearchHit hit : docs) {
-					Map<String,Object> result = hit.getSource(); 
-					String postId = hit.getId();
-					result.put("id",postId);
-					postsFounded.add(result);
-				}
-				attributes.put("searchExecuted",true);
-				if (!postsFounded.isEmpty()) { 
-					attributes.put("results",true); 
-					attributes.put("postSearchResult",postsFounded);
-				}
-			}
-
-            client.close();
-
-            Session session = request.session(false);
-            boolean existSession = false;
-            if (session != null) existSession = true;
-            attributes.put("existSession", existSession);
-
-            return new ModelAndView(attributes,"search.mustache");
             },
             new MustacheTemplateEngine()
         );
