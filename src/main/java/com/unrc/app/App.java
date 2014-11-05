7cce604cb0ca1df32c409527593cec5c1752db5c
Angelo Chaves <angeloechaves@gmail.com>
2014-11-05 17:32:18 -0300

    [#81425376] Added Search Controller and changed App routes to use it

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -10,18 +10,6 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.HashMap;
 
-import org.elasticsearch.node.Node;
-import org.elasticsearch.client.Client;
-import static org.elasticsearch.node.NodeBuilder.*;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.action.deletebyquery.DeleteByQueryResponse;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.index.query.FilterBuilders.*;
-import org.elasticsearch.index.query.QueryBuilders.*;
-import org.elasticsearch.index.query.*;
-
 import com.unrc.app.MustacheTemplateEngine;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
@@ -44,6 +32,7 @@ import com.unrc.app.controllers.AnswerController;
 import com.unrc.app.controllers.PostController;
 import com.unrc.app.controllers.QuestionController;
 import com.unrc.app.controllers.UserController;
+import com.unrc.app.controllers.SearchController;
 
 
 public class App {
@@ -66,6 +55,7 @@ public class App {
         /* - CONTROLLER CREATIONS - */
         UserController userController = new UserController();
         QuestionController question = new QuestionController();
+        SearchController searchController = new SearchController();
 
 
         before((request, response) -> {
@@ -266,59 +256,14 @@ public class App {
 
         // Show a search bar
         get("/search/users",(request,response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("found?",false);
-            return new ModelAndView(attributes,"searchUsers.mustache");
+            return searchController.searchUsersForm();
         }, 
             new MustacheTemplateEngine()
         );
 
         // Search for users
         post("/search/users", (request,response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-
-            String query = request.queryParams("carsappsearch");
-
-            //Starts the elasticsearch cluster
-            Node node = nodeBuilder().local(true).clusterName("carsapp").node();
-            Client client = node.client();
-
-            //Waits until the cluster is ready
-            ClusterHealthResponse health = client.admin()
-                                            .cluster()
-                                            .prepareHealth()
-                                            .setWaitForYellowStatus()
-                                            .execute()
-                                            .actionGet();
-
-            //Executes the search
-            SearchResponse res = client.prepareSearch("users")
-                                        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                                        .setQuery(QueryBuilders.matchQuery("name",query))
-                                        .execute()
-                                        .actionGet();
-
-            //Gets the search results
-            SearchHit[] docs = res.getHits().getHits();
-
-            //Closes the cluster
-            node.close();
-
-            Map<String,Object> map = new HashMap<String,Object>();
-            List<Map<String,Object>> userList = new LinkedList<Map<String,Object>>();
-            for (SearchHit sh : docs) {
-                map = sh.getSource();
-                userList.add(map);
-
-            } //Puts all the results in a list to be treated in the mustache
-
-            long hits = res.getHits().getTotalHits();
-
-            attributes.put("result",userList);
-            attributes.put("result_count",hits);
-            if (hits > 0) { attributes.put("found?",true); }
-
-            return new ModelAndView(attributes,"searchUsers.mustache");
+            return searchController.searchUsers(request,response);
         },
             new MustacheTemplateEngine()
         );
@@ -326,9 +271,7 @@ public class App {
 
         // Show a search bar for posts
         get("/search/posts",(request,response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("found?",false);
-            return new ModelAndView(attributes,"searchPosts.mustache");
+            return searchController.searchPostsForm();
         }, 
             new MustacheTemplateEngine()
         );
@@ -336,97 +279,7 @@ public class App {
 
         // Search for posts
         post("/search/posts", (request,response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-
-            String description = request.queryParams("postSearch");
-            String minPrice = request.queryParams("minPrice");
-            String maxPrice = request.queryParams("maxPrice");
-
-            //Starts the elasticsearch cluster
-            Node node = nodeBuilder().local(true).clusterName("carsapp").node();
-            Client client = node.client();
-
-            //Waits until the cluster is ready
-            ClusterHealthResponse health = client.admin()
-                                            .cluster()
-                                            .prepareHealth()
-                                            .setWaitForYellowStatus()
-                                            .execute()
-                                            .actionGet();
-
-            SearchResponse res = new SearchResponse();
-
-            if (minPrice.equals("") && maxPrice.equals("")) {
-                //Executes the search only with the description
-                res = client.prepareSearch("posts")
-                            .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                            .setQuery(QueryBuilders.matchQuery("description",description))
-                            .execute()
-                            .actionGet();
-            } else {
-                if (minPrice.equals("") && !(maxPrice.equals(""))) {
-                    //Executes the search with minimun price range and description
-                    res = client.prepareSearch("posts")
-                                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                                .setQuery(QueryBuilders.boolQuery()
-                                                       .must(QueryBuilders.matchQuery("description",description))
-                                                       .must(QueryBuilders.rangeQuery("price")
-                                                                          .lte(maxPrice)))
-                                .execute()
-                                .actionGet();
-                } else {
-                    if (!(minPrice.equals("")) && maxPrice.equals("")) {
-                        //Executes the search with maximun price range and description
-                        res = client.prepareSearch("posts")
-                                    .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                                    .setQuery(QueryBuilders.boolQuery()
-                                                       .must(QueryBuilders.matchQuery("description",description))
-                                                       .must(QueryBuilders.rangeQuery("price")
-                                                                          .gte(minPrice)))
-                                    .execute()
-                                    .actionGet();
-                    } else {
-                        if (!(minPrice.equals("")) && !(maxPrice.equals(""))) {
-                            //Executes the search with both price ranges and description
-                            res = client.prepareSearch("posts")
-                                        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                                        .setQuery(QueryBuilders.boolQuery()
-                                                       .must(QueryBuilders.matchQuery("description",description))
-                                                       .must(QueryBuilders.rangeQuery("price")
-                                                                          .from(minPrice)
-                                                                          .to(maxPrice)
-                                                                          .includeLower(true)
-                                                                          .includeUpper(true)))
-                                        .execute()
-                                        .actionGet();
-                        }
-                    }
-                }
-            }
-
-            //Gets the search results
-            SearchHit[] docs = res.getHits().getHits();
-
-            //Closes the cluster
-            node.close();
-
-            Map<String,Object> map = new HashMap<String,Object>();
-            List<Map<String,Object>> postList = new LinkedList<Map<String,Object>>();
-            for (SearchHit sh : docs) {
-                map = sh.getSource();
-                postList.add(map);
-
-            } //Puts all the results in a list to be treated in the mustache
-
-            long hits = res.getHits().getTotalHits();
-
-            if (hits > 0) { 
-                attributes.put("found?",true);
-                attributes.put("result",postList);
-                attributes.put("result_count",hits);
-            }
-
-            return new ModelAndView(attributes,"searchPosts.mustache");
+            return searchController.searchPosts(request,response);
         },
             new MustacheTemplateEngine()
         );
