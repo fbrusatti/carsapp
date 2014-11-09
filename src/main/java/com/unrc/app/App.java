e1f9ad74044838f6507a21390926969b61a09f36
facumolina <facu.molina7@gmail.com>
2014-11-09 13:32:01 -0300

    Minor changes

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -14,12 +14,17 @@ import spark.ModelAndView;
 import static spark.Spark.*;
 
 
-
-
 public class App {
     
+    private static UserController userController = new UserController();
+    private static SearchController searchController = new SearchController();
+    private static PostController postController = new PostController();
+    private static VehicleController vehicleController = new VehicleController();
+    private static AnswerController answerController = new AnswerController();
+    private static QuestionController questionController = new QuestionController();
+
 	public static void main( String[] args) {
-            
+
 		/**
 		 * Open and close de database.
 		 */
@@ -49,7 +54,6 @@ public class App {
          * Getting search
          */
         get("/search", (request, response) -> {
-            SearchController searchController = new SearchController();
             return searchController.getSearchView(request);
             },
             new MustacheTemplateEngine()
@@ -59,9 +63,7 @@ public class App {
          * Realizing a search
          */
         post("/search", (request, response) -> {
-            SearchController searchController = new SearchController();
             return searchController.postSearchView(request);
-            
             },
             new MustacheTemplateEngine()
         );
@@ -70,7 +72,6 @@ public class App {
          * Getting login
          */
         get("/login", (request, response) -> {
-            UserController userController = new UserController();
             return userController.getLoginView(request);
             },
             new MustacheTemplateEngine()
@@ -81,7 +82,6 @@ public class App {
          * Post login
          */
         post("/login", (request, response) -> {
-            UserController userController = new UserController();
             userController.login(request,response);
             return null; 
         });
@@ -103,7 +103,6 @@ public class App {
          * Getting users
          */
 		get("/users", (request, response) -> {
-            UserController userController = new UserController();
             return userController.getUsersView(request);
 			},
 			new MustacheTemplateEngine()
@@ -114,7 +113,6 @@ public class App {
          * Getting user by id
          */
         get("/users/:id", (request, response) -> {
-            UserController userController = new UserController();
             return userController.getUserView(request);
 			},
 			new MustacheTemplateEngine()
@@ -124,7 +122,6 @@ public class App {
          *Adding a new User 
          */
         get("newUser", (request, response) -> {
-            UserController userController = new UserController();
             return userController.getAddView(request);
             },
             new MustacheTemplateEngine()
@@ -134,7 +131,6 @@ public class App {
          * Posting a new user
          */
         post("/users", (request, response) -> {
-            UserController userController = new UserController();
             userController.add(request,response);
             return null; 
             }   
@@ -144,7 +140,6 @@ public class App {
          * Editing a user
          */ 
         get("/users/:id/edit", (request,response) -> {
-            UserController userController = new UserController();
             return userController.getEditView(request); 
             },
             new MustacheTemplateEngine()
@@ -154,7 +149,6 @@ public class App {
          * Posting a user edited
          */       
         post("/users/:id/edit", (request,response) -> {
-            UserController userController = new UserController();
             userController.edit(request,response);
             return null;
         });
@@ -163,7 +157,6 @@ public class App {
          * Deleting a user
          */
         post("users/:id/delete", (request, response) -> {
-            UserController userController = new UserController();
             userController.delete(request,response);
             return null;
         });
@@ -172,7 +165,6 @@ public class App {
          * Getting all the posts
          */
         get("/posts", (request, response) -> {
-            PostController postController = new PostController();
                 return postController.getPosts(request);
         },
         new MustacheTemplateEngine()
@@ -182,7 +174,6 @@ public class App {
          * Getting all the posts of a user
          */
         get("/users/:id/posts", (request, response) -> {
-            PostController postController = new PostController();
             return postController.getPostsUser(request);
             },
             new MustacheTemplateEngine()
@@ -192,7 +183,6 @@ public class App {
          * Getting a post of a user
          */
         get("users/:id/posts/:postId", (request, response) -> {
-            PostController postController = new PostController();
             return postController.getPost(request);
             },
             new MustacheTemplateEngine()
@@ -202,7 +192,6 @@ public class App {
          * Adding a new Post 
          */
         get("/users/:id/newPost", (request,response) -> {
-            PostController postController = new PostController();
             return postController.getNewPost(request);
             },
             new MustacheTemplateEngine()
@@ -212,7 +201,6 @@ public class App {
          * Posting a new Post  
          */
         post("/users/:id/newPost", (request, response) -> {
-            PostController postController = new PostController();
             postController.newPost(request, response);
             return null;
         });
@@ -221,7 +209,6 @@ public class App {
          * Editing a post
          */ 
         get("/users/:id/posts/:postId/edit", (request,response) -> {
-            PostController postController = new PostController();
             return postController.getEditPost(request);
             },
             new MustacheTemplateEngine()
@@ -231,7 +218,6 @@ public class App {
          * Posting a post edited
          */       
          post("/users/:id/posts/:postId/edit", (request,response) -> {
-            PostController postController = new PostController();
             postController.editPost(request, response);
             return null;
         });
@@ -240,7 +226,6 @@ public class App {
          * Deleting a post of a user
          */
         post("users/:id/posts/:postId/delete", (request, response) -> {
-            PostController postController = new PostController();
             postController.deletePost(request, response);
             return null;
         });
@@ -249,7 +234,6 @@ public class App {
          * Getting vehicles of a User
          */ 
         get("/users/:id/vehicles", (request, response) -> {
-            VehicleController vehicleController = new VehicleController();
             return vehicleController.getUserVehiclesView(request);
             },
         	new MustacheTemplateEngine()
@@ -259,7 +243,6 @@ public class App {
          * Adding a new Vehicle           
          */
         get("/users/:id/newVehicle", (request,response) -> {
-            VehicleController vehicleController = new VehicleController();
             return vehicleController.getAddView(request);
             },
             new MustacheTemplateEngine()
@@ -269,7 +252,6 @@ public class App {
          * Posting a new Vehicle 
 		 */
         post("users/:id/newVehicle", (request, response) -> {
-            VehicleController vehicleController = new VehicleController();
             vehicleController.add(request,response);
             return null;
 		});  
@@ -278,7 +260,6 @@ public class App {
          * Getting the answers of a question
          */
         get("users/:userId/posts/:postId/question/:idQuestion", (request, response) -> {
-            AnswerController answerController = new AnswerController();
             return answerController.getAnswersOfQuestionView(request);
             },
             new MustacheTemplateEngine()
@@ -288,7 +269,6 @@ public class App {
          * Posting a new question 
          */
         post("users/:id/posts/:postId/newQuestion", (request, response) -> {
-            QuestionController questionController = new QuestionController();
             questionController.add(request,response);
             return null;
             }
@@ -298,12 +278,10 @@ public class App {
          * Posting a new answer 
          */
          post("users/:id/posts/:postId/question/:questionId/newAnswer", (request, response) -> {
-            AnswerController answerController = new AnswerController();
             answerController.add(request,response);
             return null;
             }
 		);
-        
         
     }  
 }
